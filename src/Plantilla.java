c82e19aebf87df5848cd8d5258136d916b29d896
Diego <diego.garcia.santos22@estudiantes.uva.es>
2024-04-25 12:56:54 +0200

    dfg

---
+++ src/Plantilla.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Plantilla extends JPanel {
+    public Plantilla(){
+        setSize(700, 800);
+        setBackground(Color.decode("#475C8D"));
+
+    }
+
+}
